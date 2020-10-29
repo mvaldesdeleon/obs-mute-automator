599100f4588db8826892b448099c5552d4dfbdf3
Martin Valdes de Leon <m.valdesdeleon@gmail.com>
2020-10-30 00:08:28 +0100

    feat: mute indicator

--- mute-automator.py
+++ mute-automator.py
@@ -11,6 +11,117 @@ def dprint(*input):
 
 # ------------------------------------------------------------
 
+mute_indicator = ''
+
+def list_video_sources():
+    video_sources = []
+    sources = obs.obs_enum_sources()
+
+    for source in sources:
+        if obs.obs_source_get_type(source) == obs.OBS_SOURCE_TYPE_INPUT:
+            capabilities = obs.obs_source_get_output_flags(source)
+            has_video = capabilities & obs.OBS_SOURCE_VIDEO
+
+            if has_video:
+                video_sources.append(obs.obs_source_get_name(source))
+
+    obs.source_list_release(sources)
+
+    return video_sources
+
+# ------------------------------------------------------------
+
+main_microphone = 'AT2035'
+
+def list_audio_sources():
+    audio_sources = []
+    sources = obs.obs_enum_sources()
+
+    for source in sources:
+        if obs.obs_source_get_type(source) == obs.OBS_SOURCE_TYPE_INPUT:
+            capabilities = obs.obs_source_get_output_flags(source)
+            has_audio = capabilities & obs.OBS_SOURCE_AUDIO
+
+            if has_audio:
+                audio_sources.append(obs.obs_source_get_name(source))
+
+    obs.source_list_release(sources)
+
+    return audio_sources
+
+monitored_source = None
+
+def update_mute_indicator(muted):
+    if not mute_indicator:
+        return
+
+    source = obs.obs_get_source_by_name(mute_indicator)
+    if not source:
+        dprint(f'Error! Could not get source {mute_indicator}')
+        return
+
+    obs.obs_source_set_enabled(source, muted)
+
+    obs.obs_source_release(source)
+
+def mute_callback(calldata):
+    muted = obs.calldata_bool(calldata, 'muted')
+    status = 'muted' if muted else 'unmuted'
+
+    dprint(f'Main microphone {status}')
+
+    update_mute_indicator(muted)
+
+def remove_muted_callback(name):
+    if not name:
+        return
+
+    source = obs.obs_get_source_by_name(name)
+    if not source:
+        dprint(f'Error! Could not get source {name}')
+        return
+
+    handler = obs.obs_source_get_signal_handler(source)
+    obs.signal_handler_disconnect(handler, 'mute', mute_callback)
+
+    dprint(f'Removed mute callback for {name}')
+
+    obs.obs_source_release(source)
+
+    return True
+
+def create_muted_callback(name):
+    global monitored_source
+
+    if not name or monitored_source == name:
+        return
+
+    if monitored_source:
+        remove_muted_callback(monitored_source)
+
+    source = obs.obs_get_source_by_name(name)
+    if not source:
+        dprint(f'Error! Could not get source {name}')
+        return
+
+    handler = obs.obs_source_get_signal_handler(source)
+    obs.signal_handler_connect(handler, 'mute', mute_callback)
+    monitored_source = name
+
+    dprint(f'Added mute callback for {name}')
+
+    muted = obs.obs_source_muted(source)
+    status = 'muted' if muted else 'unmuted'
+
+    dprint(f'Main microphone {status}')
+
+    update_mute_indicator(muted)
+
+    obs.obs_source_release(source)
+
+
+# ------------------------------------------------------------
+
 header_decorator = '--------'
 header_pattern = '- '
 
@@ -59,26 +170,7 @@ def try_fetch_scenes():
     if fetch_scenes():
         obs.remove_current_callback()
         scenes_loaded = True
-
-# ------------------------------------------------------------
-
-main_microphone = 'AT2035'
-
-def list_audio_sources():
-    audio_sources = []
-    sources = obs.obs_enum_sources()
-
-    for source in sources:
-        if obs.obs_source_get_type(source) == obs.OBS_SOURCE_TYPE_INPUT:
-            capabilities = obs.obs_source_get_output_flags(source)
-            has_audio = capabilities & obs.OBS_SOURCE_AUDIO
-
-            if has_audio:
-                audio_sources.append(obs.obs_source_get_name(source))
-
-    obs.source_list_release(sources)
-
-    return audio_sources
+        create_muted_callback(main_microphone)
 
 # ------------------------------------------------------------
 
@@ -98,23 +190,16 @@ def frontend_cb(event):
 
         obs.obs_source_release(source)
 
-# def activate_cb(name, calldata):
-#     dprint(name + ' on')
-
-# def deactivate_cb(name, calldata):
-#     dprint(name + ' off')
-
 # ------------------------------------------------------------
 
 def script_load(settings):
     obs.obs_frontend_add_event_callback(frontend_cb)
     obs.timer_add(try_fetch_scenes, 500)
 
-    # source = obs.obs_get_source_by_name(main_microphone)
-    # obs.obs_source_release(source)
-
 def script_unload():
     obs.timer_remove(try_fetch_scenes)
+    if monitored_source is not None:
+        remove_muted_callback(monitored_source)
     obs.obs_frontend_remove_event_callback(frontend_cb)
 
 def script_description():
@@ -126,17 +211,22 @@ def script_description():
         '<a href="http://github.com/mvaldesdeleon/obs-mute-automator">github.com/mvaldesdeleon/obs-mute-automator</a>'
 
 def script_update(settings):
-    global header_decorator, header_pattern, main_microphone, debug
+    global header_decorator, header_pattern, main_microphone, mute_indicator, debug
 
     header_decorator = obs.obs_data_get_string(settings, 'header-decorator')
     header_pattern = obs.obs_data_get_string(settings, 'header-pattern')
     main_microphone = obs.obs_data_get_string(settings, 'main-microphone')
+    mute_indicator = obs.obs_data_get_string(settings, 'mute-indicator')
     debug = obs.obs_data_get_bool(settings, 'debug')
+
+    if scenes_loaded:
+        create_muted_callback(main_microphone)
 
 def script_defaults(settings):
     obs.obs_data_set_default_string(settings, 'header-decorator', header_decorator)
     obs.obs_data_set_default_string(settings, 'header-pattern', header_pattern)
     obs.obs_data_set_default_string(settings, 'main-microphone', main_microphone)
+    obs.obs_data_set_default_string(settings, 'mute-indicator', mute_indicator)
     obs.obs_data_set_default_bool(settings, 'debug', debug)
 
 def script_properties():
@@ -146,9 +236,13 @@ def script_properties():
     obs.obs_properties_add_text(props, 'header-pattern', 'List of characters used in the decorator', obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_button(props, 'reload-scenes', 'Reload scenes', lambda *args: fetch_scenes())
 
-    source_list = obs.obs_properties_add_list(props, 'main-microphone', 'Main microphone', obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    audio_source_list = obs.obs_properties_add_list(props, 'main-microphone', 'Main microphone', obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     for name in list_audio_sources():
-        obs.obs_property_list_add_string(source_list, name, name)
+        obs.obs_property_list_add_string(audio_source_list, name, name)
+
+    video_source_list = obs.obs_properties_add_list(props, 'mute-indicator', 'Mute indicator', obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    for name in list_video_sources():
+        obs.obs_property_list_add_string(video_source_list, name, name)
 
     obs.obs_properties_add_bool(props, 'debug', 'Print Debug Messages')
     
